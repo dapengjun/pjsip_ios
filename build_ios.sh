d8d92850d080ddb71b2a00501d776fc3c666a3d6
lixiangjun <lixiangjun@sursen.net>
2016-02-25 11:23:24 +0800

    initial commit pjsip 2.4.5

---
+++ build_ios.sh
@@ -0,0 +1,7 @@
+#./configure-iphone --with-ssl=/Users/fengshen/workspace/ios/openssl_lib_ios
+#ARCH='-arch armv7s' ./configure-iphone --with-ssl=/Users/fengshen/workspace/ios/openssl_lib_ios
+#ARCH='-arch arm64' ./configure-iphone --with-ssl=/Users/fengshen/workspace/ios/openssl_lib_ios
+
+export DEVPATH=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer
+ARCH='-arch i386' ./configure-iphone --with-ssl=/Users/fengshen/workspace/ios/openssl_lib_ios
+
